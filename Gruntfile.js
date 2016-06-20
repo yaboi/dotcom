368c371af74b1e33a21b8c9f1ba5a60f935ac430
Tyler Goelz <tyler@tylergoelz.com>
2016-06-20 13:56:51 +0000

    update grunt to check if heroku still works

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function(grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 3000,
+                port: 8080,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: '0.0.0.0',
                 livereload: false,
